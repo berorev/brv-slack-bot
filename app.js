bbde0c86b027c3c2ae06cf7595941a3792afffaa
berorev <berorev@gmail.com>
2019-05-15 22:51:36 +0900

    port

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, '0.0.0.0', () => {
-  console.log('brv-slack-bot listening on port 3000.');
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`brv-slack-bot listening on port ${port}.`);
 });
